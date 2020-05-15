01fa7cc1ddd4f285edfe6dc2c40ae9e635bc08fc
Randheer Kumar <randheer.kumar@randheerkumars-MBP.local>
2020-05-16 00:54:04 +0800

    basic information added

---
+++ src/pages/home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+function Home() {
+    return (
+        <div className="Home">
+            <header className="Home-Header">
+                Randheer Kumar Singh
+            </header>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/resources/Constants.js
@@ -0,0 +1,6 @@
+export const Colors = {
+    Theme: '#55aaff',
+    Background: '#ffffff',
+    TextOnTheme: '#ffffff',
+    TextOnBackground: '#1c1b1b',
+}
